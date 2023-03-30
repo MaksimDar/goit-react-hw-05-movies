7868552f2e04275a8341348b3fc449a4408e7d65
Maksim1903 <dovgusha.m@gmail.com>
2023-03-30 20:04:13 +0200

    Update CustomLink.jsx

--- src/components/CustomLink/CustomLink.jsx
+++ src/components/CustomLink/CustomLink.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useMatch } from 'react-router-dom';
-import styled from '@emotion/styled';
+import styled from 'styled-components';
 
 const CustomLinkStyled = styled(Link)`
   color: ${props =>
